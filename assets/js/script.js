f3617731a1e557665fd5e7829efe1a28219cd521
Caleb Crouch <cecrouch01@gmail.com>
2023-06-25 20:06:54 -0400

    creation of variables which connect to class

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//We need to be able to put question in the question area
+//We need to have answers that correspond with the question
+//Oonce that is done look at adding arrays to arrays
+//use a randomizer to put the array in the question
+
+var question = document.querySelector(".question")
+var answerA = document.querySelector(".answer-a")
+var answerB = document.querySelector(".answer-b")
+var answerC = document.querySelector(".answer-c")
+var answerD = document.querySelector(".answer-d")
+
+question.innerText = "Hello World?"
+console.log(answerA.textContent)
+console.log(answerB)
+console.log(answerC)
+console.log(answerD)
